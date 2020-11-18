f96e666002d2e225efc9f3238a32a866998ac118
Dan Ibanez <daibane@sandia.gov>
2020-11-18 11:56:21 -0700

    remove "inline" from ALWAYS_INLINE...

--- p3a_macros.hpp
+++ p3a_macros.hpp
@@ -4,7 +4,7 @@
 #define CPL_ALWAYS_INLINE __forceinline
 #define CPL_NEVER_INLINE __declspec(noinline)
 #elif defined(__CUDACC__)
-#define CPL_ALWAYS_INLINE __attribute__((always_inline)) inline
+#define CPL_ALWAYS_INLINE __attribute__((always_inline))
 #define CPL_NEVER_INLINE __attribute__((noinline))
 #else
 #define CPL_ALWAYS_INLINE __attribute__((always_inline))
