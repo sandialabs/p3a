895c36a54a39b5ed3fec74067685a234a81c0ef3
Dan Ibanez <daibane@sandia.gov>
2020-11-20 09:40:59 -0700

    make static_array host+device

--- p3a_static_array.hpp
+++ p3a_static_array.hpp
@@ -13,27 +13,27 @@ class static_array {
   using size_type = int;
   using iterator = T*;
   using const_iterator = T const*;
-  [[nodiscard]] P3A_ALWAYS_INLINE constexpr
+  [[nodiscard]] P3A_HOST P3A_DEVICE P3A_ALWAYS_INLINE constexpr
   reference operator[](size_type pos) { return m_members[pos]; }
-  [[nodiscard]] P3A_ALWAYS_INLINE constexpr
+  [[nodiscard]] P3A_HOST P3A_DEVICE P3A_ALWAYS_INLINE constexpr
   const_reference operator[](size_type pos) const { return m_members[pos]; }
-  [[nodiscard]] P3A_ALWAYS_INLINE static constexpr
+  [[nodiscard]] P3A_HOST P3A_DEVICE P3A_ALWAYS_INLINE static constexpr
   size_type size() { return N; }
-  [[nodiscard]] P3A_ALWAYS_INLINE constexpr
+  [[nodiscard]] P3A_HOST P3A_DEVICE P3A_ALWAYS_INLINE constexpr
   T* data() { return m_members; }
-  [[nodiscard]] P3A_ALWAYS_INLINE constexpr
+  [[nodiscard]] P3A_HOST P3A_DEVICE P3A_ALWAYS_INLINE constexpr
   T const* data() const { return m_members; }
-  [[nodiscard]] P3A_ALWAYS_INLINE constexpr
+  [[nodiscard]] P3A_HOST P3A_DEVICE P3A_ALWAYS_INLINE constexpr
   iterator begin() { return data(); }
-  [[nodiscard]] P3A_ALWAYS_INLINE constexpr
+  [[nodiscard]] P3A_HOST P3A_DEVICE P3A_ALWAYS_INLINE constexpr
   const_iterator begin() const { return data(); }
-  [[nodiscard]] P3A_ALWAYS_INLINE constexpr
+  [[nodiscard]] P3A_HOST P3A_DEVICE P3A_ALWAYS_INLINE constexpr
   const_iterator cbegin() const { return data(); }
-  [[nodiscard]] P3A_ALWAYS_INLINE constexpr
+  [[nodiscard]] P3A_HOST P3A_DEVICE P3A_ALWAYS_INLINE constexpr
   iterator end() { return data() + size(); }
-  [[nodiscard]] P3A_ALWAYS_INLINE constexpr
+  [[nodiscard]] P3A_HOST P3A_DEVICE P3A_ALWAYS_INLINE constexpr
   const_iterator end() const { return data() + size(); }
-  [[nodiscard]] P3A_ALWAYS_INLINE constexpr
+  [[nodiscard]] P3A_HOST P3A_DEVICE P3A_ALWAYS_INLINE constexpr
   const_iterator cend() const { return data() + size(); }
 };
 
