8dae09a14662289870e76be23431954f283962ce
Dan Ibanez <daibane@sandia.gov>
2020-11-23 13:12:05 -0700

    replace local with device_local

--- p3a_execution.hpp
+++ p3a_execution.hpp
@@ -9,10 +9,10 @@ class serial_execution {
 
 inline constexpr serial_execution serial = {};
 
-class local_execution {
+class serial_local_execution {
 };
 
-inline constexpr local_execution local = {};
+inline constexpr serial_local_execution serial_local = {};
 
 #ifdef __CUDACC__
 
@@ -21,13 +21,21 @@ class cuda_execution {
 
 inline constexpr cuda_execution cuda = {};
 
+class cuda_local_execution {
+};
+
+inline constexpr cuda_local_execution cuda_local = {};
+
 #endif
 
 #ifdef __CUDACC__
 using device_execution = cuda_execution;
+using device_local_execution = cuda_local_execution;
 #else
 using device_execution = serial_execution;
+using device_local_execution = serial_local_execution;
 #endif
 inline constexpr device_execution device = {};
+inline constexpr device_local_execution device_local = {};
 
 }
