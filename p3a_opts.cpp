c4faea2d7f10e4a4ed6b7e2cedccac2d5d9194f9
Dan Ibanez <daibane@sandia.gov>
2021-05-27 16:35:26 -0600

    Add command line options tool

---
+++ p3a_opts.cpp
@@ -0,0 +1,262 @@
+#include "p3a_opts.hpp"
+
+#include <queue>
+
+namespace p3a {
+
+opt::opt(std::string const& name_arg)
+  :m_name(name_arg)
+{
+  for (char c : m_name) {
+    if (!std::isalnum(c) && !(c == '-')) {
+      throw opt_error(
+          "option name " +
+          m_name +
+          " contains more than " +
+          "letters, numbers, and dashes");
+    }
+  }
+}
+
+std::string const& opt::name() const
+{
+  return m_name;
+}
+
+opt& opt::expect_arguments(int n)
+{
+  m_expected_argument_count = n;
+  return *this;
+}
+
+int opt::expected_argument_count() const
+{
+  return m_expected_argument_count;
+}
+
+void opt::set()
+{
+  if (m_is_set) {
+    throw opt_error(
+        "option " +
+        m_name +
+        " was set twice");
+  }
+  m_is_set = true;
+}
+
+bool opt::is_set() const
+{
+  return m_is_set;
+}
+
+int opt::argument_count() const
+{
+  return int(m_arguments.size());
+}
+
+void opt::add_argument(std::string const& arg)
+{
+  if (m_expected_argument_count != -1 &&
+      m_expected_argument_count == argument_count()) {
+    throw opt_error(
+        "option " +
+        m_name +
+        " given too many arguments, expects " +
+        std::to_string(m_expected_argument_count));
+  }
+  m_arguments.push_back(arg);
+}
+
+std::string const& opt::argument(int i) const
+{
+  if (!m_is_set) {
+    throw opt_error(
+        "argument requested from option " +
+        m_name +
+        " but that option was not given");
+  }
+  if (i < 0) {
+    throw opt_error(
+        "option " +
+        m_name +
+        " negative argument index requested");
+  }
+  if (i >= argument_count()) {
+    throw opt_error(
+        "option " +
+        m_name +
+        " argument index out of range " +
+        std::to_string(i) +
+        " >= " +
+        std::to_string(argument_count()));
+  }
+  return m_arguments[std::size_t(i)];
+}
+
+opt& opts::add(std::string const& name)
+{
+  for (opt const& o : m_options) {
+    if (o.name() == name) {
+      throw opt_error(
+          "can't add option " +
+          name +
+          " because it already exists");
+    }
+  }
+  for (opt const& o : m_positional_options) {
+    if (o.name() == name) {
+      throw opt_error(
+          "can't add option " +
+          name +
+          " because it already exists");
+    }
+  }
+  m_options.push_back(opt(name));
+  return m_options.back();
+}
+
+opt& opts::add_positional(std::string const& name)
+{
+  for (opt const& o : m_options) {
+    if (o.name() == name) {
+      throw opt_error(
+          "can't add positional option " +
+          name +
+          " because it already exists");
+    }
+  }
+  for (opt const& o : m_positional_options) {
+    if (o.name() == name) {
+      throw opt_error(
+          "can't add positional option " +
+          name +
+          " because it already exists");
+    }
+  }
+  m_positional_options.push_back(opt(name));
+  return m_positional_options.back();
+}
+
+void opts::parse(int& argc, char** argv)
+{
+  std::queue<char*> arguments;
+  for (int i = 1; i < argc; ++i) {
+    arguments.push(argv[i]);
+  }
+  opt* option_receiving_arguments = nullptr;
+  try {
+    while (!arguments.empty()) {
+      char* c_argument = arguments.front();
+      arguments.pop();
+      std::string const argument(c_argument);
+      if (option_receiving_arguments->argument_count() ==
+          option_receiving_arguments->expected_argument_count()) {
+        option_receiving_arguments = nullptr;
+      }
+      if (option_receiving_arguments) {
+        if (option_receiving_arguments->expected_argument_count() == -1) {
+          bool was_new_option = false;
+          for (opt& o : m_options) {
+            if (argument == "--" + o.name()) {
+              o.set();
+              was_new_option = true;
+              option_receiving_arguments = &o;
+            }
+          }
+          if (!was_new_option) {
+            option_receiving_arguments->add_argument(argument);
+          }
+        } else {
+          option_receiving_arguments->add_argument(argument);
+        }
+      } else {
+        bool was_new_option = false;
+        for (opt& o : m_options) {
+          if (argument == "--" + o.name()) {
+            o.set();
+            option_receiving_arguments = &o;
+            was_new_option = true;
+          }
+        }
+        if (!was_new_option) {
+          bool was_given_to_positional = false;
+          for (opt& o : m_positional_options) {
+            if (o.argument_count() != o.expected_argument_count()) {
+              o.set();
+              option_receiving_arguments = &o;
+              o.add_argument(argument);
+              was_given_to_positional = true;
+            }
+          }
+          if (!was_given_to_positional) {
+            throw opt_error(
+                "unrecognized command line argument " +
+                argument);
+          }
+        }
+      }
+    }
+    for (opt const& o : m_options) {
+      if (o.expected_argument_count() != -1 &&
+          o.expected_argument_count() != o.argument_count()) {
+        throw opt_error(
+            "option " +
+            o.name() +
+            " expected " +
+            std::to_string(o.expected_argument_count()) +
+            " arguments but received " +
+            std::to_string(o.argument_count()));
+      }
+    }
+    for (opt const& o : m_positional_options) {
+      if (o.expected_argument_count() != -1 &&
+          o.expected_argument_count() != o.argument_count()) {
+        throw opt_error(
+            "option " +
+            o.name() +
+            " expected " +
+            std::to_string(o.expected_argument_count()) +
+            " arguments but received " +
+            std::to_string(o.argument_count()));
+      }
+    }
+  } catch (opt_error const& e) {
+    throw opt_error(std::string(e.what()) + "\n" + help_text());
+  }
+}
+
+bool opts::has(std::string const& name) const
+{
+  return get_option(name).is_set();
+}
+
+int opts::argument_count(std::string const& name) const
+{
+  return get_option(name).argument_count();
+}
+
+std::string const& opts::argument(std::string const& name, int i) const
+{
+  return get_option(name).argument(i);
+}
+
+opt const& opts::get_option(std::string const& name) const
+{
+  for (opt const& o : m_options) {
+    if (o.name() == name) {
+      return o;
+    }
+  }
+  for (opt const& o : m_positional_options) {
+    if (o.name() == name) {
+      return o;
+    }
+  }
+  throw opt_error(
+      "option " +
+      name +
+      " requested but was never defined");
+}
+
+}
---
+++ p3a_opts.hpp
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <sstream>
+#include <stdexcept>
+
+namespace p3a {
+
+class opt_error : public std::runtime_error {
+ public:
+  opt_error(std::string const& arg)
+    :std::runtime_error(arg)
+  {}
+};
+
+class opt {
+  std::string m_name;
+  std::vector<std::string> m_arguments;
+  bool m_is_set{false};
+  int m_expected_argument_count{-1};
+ public:
+  opt(std::string const& name_arg);
+  std::string const& name() const;
+  opt& expect_arguments(int n = -1);
+  int expected_argument_count() const;
+  void set();
+  bool is_set() const;
+  int argument_count() const;
+  void add_argument(std::string const& arg);
+  std::string const& argument(int i) const;
+  template <class T>
+  T argument_as(int i) const
+  {
+    std::stringstream stream(argument(i));
+    T result;
+    stream >> result;
+    if (stream.fail() || !stream.eof()) {
+      throw opt_error(
+          "option " +
+          m_name +
+          " argument " +
+          argument(i) +
+          " is not convertible to the desired type");
+    }
+    return result;
+  }
+};
+
+class opts {
+  std::vector<opt> m_options;
+  std::vector<opt> m_positional_options;
+ public:
+  opt& add(std::string const& name);
+  opt& add_positional(std::string const& name);
+  void parse(int& argc, char** argv);
+  bool has(std::string const& name) const;
+  int argument_count(std::string const& name) const;
+  std::string const& argument(std::string const& name, int i = 0) const;
+  template <class T>
+  T argument_as(std::string const& name, int i = 0) const
+  {
+    try {
+      return get_option(name).argument_as<T>(i);
+    } catch (opt_error const& e) {
+      throw opt_error(std::string(e.what()) + "\n" + help_text());
+    }
+  }
+ private:
+  opt const& get_option(std::string const& name) const;
+  std::string help_text() const;
+};
+
+}
