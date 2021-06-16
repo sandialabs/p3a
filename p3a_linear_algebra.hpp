c0f951ad8232fef214eb42e8646d1d08d75a5b9f
Dan Ibanez <daibane@sandia.gov>
2021-06-16 09:09:32 -0600

    remove CG solver

--- p3a_linear_algebra.hpp
+++ p3a_linear_algebra.hpp
@@ -40,25 +40,4 @@ void axpy(double a, dense_matrix const& x, dense_matrix const& y, dense_matrix& 
 void multiply(dense_matrix const& a, dense_matrix const& b, dense_matrix& result);
 void solve(dense_matrix& a, dense_matrix& b, dense_matrix& x);
 
-class conjugate_gradient_solver {
-  device_array<double> m_r;
-  device_array<double> m_p;
-  device_array<double> m_Ap;
-  reproducible_floating_point_adder m_adder;
- public:
-  using Ax_functor_type = std::function<
-    void(device_array<double> const&, device_array<double>&)>;
-  using b_functor_type = std::function<
-    void(device_array<double>&)>;
-  conjugate_gradient_solver() = default;
-  conjugate_gradient_solver(mpi::comm&& comm_arg)
-    :m_adder(std::move(comm_arg))
-  {}
-  int solve(
-      Ax_functor_type const& Ax_functor,
-      b_functor_type const& b_functor,
-      device_array<double>& x,
-      double relative_tolerance);
-};
-
 }
