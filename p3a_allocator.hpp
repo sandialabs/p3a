0eb545bd0e648e80836e0137568a8b26ee0aad0e
Dan Ibanez <daibane@sandia.gov>
2021-07-06 14:01:37 -0700

    draft HIP host and device allocators

--- p3a_allocator.hpp
+++ p3a_allocator.hpp
@@ -20,7 +20,7 @@ class allocator {
   }
 };
 
-#if defined(__CUDACC__)
+#ifdef __CUDACC__
 
 template <class T>
 class cuda_host_allocator {
@@ -58,17 +58,61 @@ class cuda_device_allocator {
 
 #endif
 
-#ifdef __CUDACC__
+#ifdef __HIPCC__
+
+template <class T>
+class hip_host_allocator {
+ public:
+  using size_type = std::int64_t;
+  template <class U> struct rebind { using other = p3a::cuda_host_allocator<U>; };
+  P3A_NEVER_INLINE static T* allocate(size_type n)
+  {
+    void* ptr = nullptr;
+    hipHostMalloc(&ptr, std::size_t(n) * sizeof(T), hipHostMallocDefault);
+    return static_cast<T*>(ptr);
+  }
+  P3A_NEVER_INLINE static void deallocate(T* p, size_type)
+  {
+    hipHostFree(p);
+  }
+};
+
+template <class T>
+class hip_device_allocator {
+ public:
+  using size_type = std::int64_t;
+  template <class U> struct rebind { using other = p3a::cuda_device_allocator<U>; };
+  P3A_NEVER_INLINE static T* allocate(size_type n)
+  {
+    void* ptr = nullptr;
+    hipMalloc(&ptr, std::size_t(n) * sizeof(T));
+    return static_cast<T*>(ptr);
+  }
+  P3A_NEVER_INLINE static void deallocate(T* p, size_type)
+  {
+    hipFree(p);
+  }
+};
+
+#endif
+
+#if defined(__CUDACC__)
 template <class T>
 using device_allocator = cuda_device_allocator<T>;
+#elif defined(__HIPCC__)
+template <class T>
+using device_allocator = hip_device_allocator<T>;
 #else
 template <class T>
 using device_allocator = allocator<T>;
 #endif
 
-#ifdef __CUDACC__
+#if defined(__CUDACC__)
 template <class T>
 using mirror_allocator = cuda_host_allocator<T>;
+#elif defined(__HIPCC__)
+template <class T>
+using mirror_allocator = hip_host_allocator<T>;
 #else
 template <class T>
 using mirror_allocator = allocator<T>;
