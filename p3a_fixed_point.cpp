3fa6beb8eff0fcef35d60161d169a6bb9a29361c
Dan Ibanez <daibane@sandia.gov>
2022-01-17 11:53:19 -0700

    remove debug code

--- p3a_fixed_point.cpp
+++ p3a_fixed_point.cpp
@@ -60,13 +60,11 @@ double fixed_point_double_sum<Allocator, ExecutionPolicy>::compute()
     std::int64_t significand;
     int exponent;
     decompose_double(value, significand, exponent);
-    printf("value %.17e exponent %d\n", value, exponent);
     return exponent;
   });
   int global_max_exponent = local_max_exponent;
   m_comm.iallreduce(
       &global_max_exponent, 1, mpi::op::max());
-  printf("global max exponent %d\n", global_max_exponent);
   int128 const local_sum =
     m_int128_reducer.transform_reduce(
         m_values.cbegin(), m_values.cend(),
@@ -74,11 +72,8 @@ double fixed_point_double_sum<Allocator, ExecutionPolicy>::compute()
         adds<int128>,
   [=] P3A_HOST P3A_DEVICE (double const& value) P3A_ALWAYS_INLINE {
     std::int64_t significand_64 = decompose_double(value, global_max_exponent);
-    printf("value %.17e ~= %lld * (2 ^ %d)\n",
-        value, significand_64, global_max_exponent);
     return int128(significand_64);
   });
-  printf("local sum high %lld low %llu\n", local_sum.high(), local_sum.low());
   int128 global_sum = local_sum;
   auto const int128_mpi_sum_op = 
     mpi::op::create(p3a_mpi_int128_sum);
@@ -88,16 +83,7 @@ double fixed_point_double_sum<Allocator, ExecutionPolicy>::compute()
       sizeof(int128),
       mpi::datatype::predefined_packed(),
       int128_mpi_sum_op);
-  printf("global sum high %lld low %llu\n", local_sum.high(), local_sum.low());
-  double const result = compose_double(global_sum, global_max_exponent);
-  printf("result %.17e\n", result);
-  double debug_sum = 0.0;
-  for (std::int64_t i = 0; i < m_values.size(); ++i) {
-    debug_sum += m_values[i];
-  }
-  printf("debug sum %.17e\n", debug_sum);
-  std::exit(-1);
-  return result;
+  return compose_double(global_sum, global_max_exponent);
 }
 
 // explicitly instantiate for host and device so the actual reduction
