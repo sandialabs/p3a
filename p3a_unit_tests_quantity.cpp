64d851b10056876cd9f68cb5c120dc5c0ecc741c
Dan Ibanez <daibane@sandia.gov>
2022-03-17 13:40:41 -0600

    fix unit tests for units (pun)

--- p3a_unit_tests_quantity.cpp
+++ p3a_unit_tests_quantity.cpp
@@ -1,8 +1,32 @@
 #include "gtest/gtest.h"
 #include "p3a_quantity.hpp"
 
+TEST(quantity, multiply) {
+  static_assert(std::is_same_v<
+      p3a::watt,
+      p3a::unit<p3a::dimension<-3, 2, 1>>>,
+      "Watt is the SI unit equal to kg * m^2 * s^-3");
+  static_assert(std::is_same_v<
+      p3a::second,
+      p3a::unit<p3a::dimension<1, 0, 0>>>,
+      "Second is the SI unit with time dimension");
+  static_assert(std::is_same_v<
+      p3a::joule,
+      p3a::unit<p3a::dimension<-2, 2, 1>>>,
+      "Joule is the SI unit equal to kg * m^2 * s^-2");
+  static_assert(std::is_same_v<
+      p3a::unit_multiply<p3a::watt, p3a::second>,
+      p3a::joule>,
+      "Watt times second = joule");
+  auto a = p3a::watts<double>(1.0) * p3a::seconds<double>(2.0);
+  static_assert(std::is_same_v<decltype(a), p3a::joules<double>>,
+      "Watts times seconds should be Joules");
+  EXPECT_FLOAT_EQ(a.value(), 2.0);
+}
+
 TEST(quantity, divide) {
   auto a = p3a::meters<double>(1.0) / p3a::seconds<double>(2.0);
   static_assert(std::is_same_v<decltype(a), p3a::meters_per_second<double>>,
       "meters times seconds should be meters per second");
+  EXPECT_FLOAT_EQ(a.value(), 0.5);
 }
