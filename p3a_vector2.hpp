ede321b388680cddfc6134a5b3b1160145690396
Dan Ibanez <daibane@sandia.gov>
2022-04-07 22:51:43 -0600

    more permissive ctor for vector2

--- p3a_vector2.hpp
+++ p3a_vector2.hpp
@@ -27,6 +27,18 @@ class vector2 {
     :vector2(T(other.x()), T(other.y()))
   {
   }
+  template <
+    class U,
+    class V,
+    typename std::enable_if<
+      std::is_constructible_v<T, U const&> &&
+      std::is_constructible_v<T, V const&>,
+      bool>::type = false>
+  P3A_HOST P3A_DEVICE P3A_ALWAYS_INLINE explicit constexpr
+  vector2(U const& a, V const& b)
+    :m_x(a)
+    ,m_y(b)
+  {}
   [[nodiscard]] P3A_HOST P3A_DEVICE P3A_ALWAYS_INLINE constexpr
   reference x() { return m_x; }
   [[nodiscard]] P3A_HOST P3A_DEVICE P3A_ALWAYS_INLINE constexpr
