8c96ee1da8093969d489c91fc2c6b9f77ecfd4c1
Dan Ibanez <daibane@sandia.gov>
2022-05-04 14:13:12 -0600

    test that we can hold fancy objects too

--- p3a_unit_tests_dynamic_array.cpp
+++ p3a_unit_tests_dynamic_array.cpp
@@ -1,9 +1,18 @@
 #include "gtest/gtest.h"
 
+#include <string>
+
 #include "p3a_dynamic_array.hpp" 
 
 TEST(dynamic_array, device_fill)
 {
   p3a::device_array<double> a;
   a.resize(100, -42.0);
+}
+
+TEST(dynamic_array, fancy_object)
+{
+  p3a::dynamic_array<std::string> a;
+  a.push_back("a");
+  a.push_back("b");
 }
