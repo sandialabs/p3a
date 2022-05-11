890d78bc79f42ff75f3fc7459bf8b16b28894d9c
Dan Ibanez <daibane@sandia.gov>
2022-05-11 16:39:10 -0600

    start trying to use SIMD from Kokkos

--- p3a_simd.hpp
+++ p3a_simd.hpp
@@ -1,41 +1,26 @@
 #pragma once
 
-#include "p3a_simd_common.hpp"
+#include <Kokkos_Simd.hpp>
 
-#include "p3a_simd_scalar.hpp"
-
-#ifdef __AVX512F__
-#include "p3a_avx512.hpp"
-#endif
-
-#include <Kokkos_Macros.hpp>
+#include "p3a_functions.hpp"
+#include "p3a_type_traits.hpp"
 
 namespace p3a {
 
-namespace simd_abi {
+using Kokkos::simd;
+using Kokkos::simd_mask;
+using Kokkos::const_where_expression;
+using Kokkos::where_expression;
+using Kokkos::element_aligned_tag;
+namespace simd_abi = Kokkos::simd_abi;
 
-#if defined(__AVX512F__)
-using host_native = avx512_fixed_size<8>;
-#else
-using host_native = scalar;
-#endif
-
-#if defined(KOKKOS_ENABLE_CUDA) || defined(KOKKOS_ENABLE_HIP)
-using device_native = scalar;
-#else
-using device_native = host_native;
-#endif
-
+template <class T, class U, class Abi>
+P3A_ALWAYS_INLINE P3A_HOST P3A_DEVICE inline
+simd<T, Abi> load(T const* ptr, int i, simd_mask<U, Abi> const& mask)
+{
+  simd<T, Abi> result;
+  where(simd_mask<T, Abi>(mask), result).copy_from(ptr + i, element_aligned_tag());
+  return result;
 }
-
-template <class T>
-using host_simd = simd<T, simd_abi::host_native>;
-template <class T>
-using device_simd = simd<T, simd_abi::device_native>;
-
-template <class T>
-using host_simd_mask = simd_mask<T, simd_abi::host_native>;
-template <class T>
-using device_simd_mask = simd_mask<T, simd_abi::device_native>;
 
 }
