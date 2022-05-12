b60a4fbe523cf5ca7ba490f744267758d20b9772
Dan Ibanez <daibane@sandia.gov>
2022-05-12 14:13:12 -0600

    add some more missing glue code (load/store etc)

--- p3a_simd.hpp
+++ p3a_simd.hpp
@@ -14,6 +14,8 @@ using Kokkos::const_where_expression;
 using Kokkos::where_expression;
 using Kokkos::element_aligned_tag;
 namespace simd_abi = Kokkos::simd_abi;
+using Kokkos::device_simd;
+using Kokkos::device_simd_mask;
 
 template <class T, class U, class Abi>
 P3A_ALWAYS_INLINE P3A_HOST P3A_DEVICE inline
@@ -22,6 +24,29 @@ simd<T, Abi> load(T const* ptr, int i, simd_mask<U, Abi> const& mask)
   simd<T, Abi> result;
   where(simd_mask<T, Abi>(mask), result).copy_from(ptr + i, element_aligned_tag());
   return result;
+}
+
+template <class T, class U, class Integral, class Abi>
+P3A_ALWAYS_INLINE P3A_HOST P3A_DEVICE inline
+simd<T, Abi> load(T const* ptr, simd<Integral, Abi> const& indices, simd_mask<U, Abi> const& mask)
+{
+  simd<T, Abi> result;
+  where(simd_mask<T, Abi>(mask), result).gather_from(ptr, indices);
+  return result;
+}
+
+template <class T, class U, class Abi>
+P3A_ALWAYS_INLINE P3A_HOST P3A_DEVICE inline
+void store(simd<T, Abi> const& value, T* ptr, int i, simd_mask<U, Abi> const& mask)
+{
+  where(simd_mask<T, Abi>(mask), value).copy_to(ptr + i, element_aligned_tag());
+}
+
+template <class T, class U, class Integral, class Abi>
+P3A_ALWAYS_INLINE P3A_HOST P3A_DEVICE inline
+void store(simd<T, Abi> const& value, T* ptr, simd<Integral, Abi> const& indices, simd_mask<U, Abi> const& mask)
+{
+  where(simd_mask<T, Abi>(mask), value).scatter_to(ptr, indices);
 }
 
 template<class M, class V, class T>
