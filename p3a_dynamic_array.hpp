3c23b5b8058b12e923ebeffcb9dfad3baee68721
Dan Ibanez <daibane@sandia.gov>
2022-06-02 13:26:32 -0600

    allow copying dynamic_array between memory spaces
    this is hopefully going to be the official way to
    copy between host and device in the future

--- p3a_dynamic_array.hpp
+++ p3a_dynamic_array.hpp
@@ -80,6 +80,39 @@ class dynamic_array {
     uninitialized_copy(m_execution_policy, other.begin(), other.end(), m_begin);
     return *this;
   }
+  template <
+    class Allocator2,
+    class ExecutionSpace2,
+    typename std::enable_if<!std::is_same_v<Allocator2, Allocator>,
+      bool>::type = false>
+  dynamic_array(dynamic_array<T, Allocator2, ExecutionSpace2> const& other)
+    :m_begin(nullptr)
+    ,m_size(0)
+    ,m_capacity(0)
+  {
+    reserve(other.capacity());
+    m_size = other.size();
+    details::copy_between_spaces(
+        other.data(),
+        this->data(),
+        std::size_t(this->size()));
+  }
+  template <
+    class Allocator2,
+    class ExecutionSpace2,
+    typename std::enable_if<!std::is_same_v<Allocator2, Allocator>,
+      bool>::type = false>
+  dynamic_array& operator=(dynamic_array<T, Allocator2, ExecutionSpace2> const& other)
+  {
+    resize(0);
+    reserve(other.capacity());
+    m_size = other.size();
+    details::copy_between_spaces(
+        other.data(),
+        this->data(),
+        std::size_t(this->size()));
+    return *this;
+  }
   explicit dynamic_array(size_type size_in)
     :dynamic_array()
   {
