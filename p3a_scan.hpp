7b0c2bd480ccd07a1e5f91123bd9014526509463
Dan Ibanez <daibane@sandia.gov>
2022-07-20 10:17:38 -0600

    added p3a::copy_if

--- p3a_scan.hpp
+++ p3a_scan.hpp
@@ -2,6 +2,8 @@
 
 #include <stdexcept>
 
+#include <Kokkos_StdAlgorithms.hpp>
+
 #include "p3a_execution.hpp"
 
 namespace p3a {
@@ -56,6 +58,65 @@ void kokkos_exclusive_scan(
       functor(first, d_first));
 }
 
+template <class InputIt, class OutputIt, class UnaryPredicate>
+class kokkos_copy_if_functor {
+  InputIt m_first;
+  OutputIt m_d_first;
+  UnaryPredicate m_pred;
+ public:
+  kokkos_copy_if_functor(
+      InputIt first_arg,
+      OutputIt d_first_arg,
+      UnaryPredicate pred_arg)
+    :m_first(first_arg)
+    ,m_d_first(d_first_arg)
+    ,m_pred(pred_arg)
+  {
+  }
+  using input_difference_type = typename std::iterator_traits<InputIt>::difference_type;
+  using output_difference_type = typename std::iterator_traits<OutputIt>::difference_type;
+  P3A_ALWAYS_INLINE P3A_HOST_DEVICE inline
+  void operator()(
+      input_difference_type const i,
+      output_difference_type& update,
+      bool const is_final_pass) const
+  {
+    if (is_final_pass) {
+      if (m_pred(m_first[i])) {
+        m_d_first[update] = m_first[i];
+      }
+    }
+    if (m_pred(m_first[i])) {
+      update += 1;
+    }
+  }
+};
+
+template <
+  class ExecutionSpace,
+  class InputIt,
+  class OutputIt,
+  class UnaryPredicate>
+OutputIt kokkos_copy_if(
+    InputIt first,
+    InputIt last,
+    OutputIt d_first,
+    UnaryPredicate pred)
+{
+  using input_difference_type = typename std::iterator_traits<InputIt>::difference_type;
+  using kokkos_policy =
+    Kokkos::RangePolicy<
+      ExecutionSpace,
+      Kokkos::IndexType<input_difference_type>>;
+  using functor = kokkos_copy_if_functor<InputIt, OutputIt, UnaryPredicate>;
+  input_difference_type copied_count = 0;
+  Kokkos::parallel_scan("p3a::details::kokkos_copy_if",
+      kokkos_policy(0, (last - first)),
+      functor(first, d_first, pred),
+      copied_count);
+  return d_first + copied_count;
+}
+
 }
 
 template <
@@ -72,6 +133,22 @@ void exclusive_scan(
 {
   details::kokkos_exclusive_scan<typename ExecutionPolicy::kokkos_execution_space>(
       first, last, d_first, init);
+}
+
+template <
+  class ExecutionPolicy,
+  class InputIt,
+  class OutputIt,
+  class UnaryPredicate>
+OutputIt copy_if(
+    ExecutionPolicy policy,
+    InputIt first,
+    InputIt last,
+    OutputIt d_first,
+    UnaryPredicate pred)
+{
+  return details::kokkos_copy_if<typename ExecutionPolicy::kokkos_execution_space>(
+      first, last, d_first, pred);
 }
 
 }
