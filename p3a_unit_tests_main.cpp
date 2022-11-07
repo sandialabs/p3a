6f82371114e74786fd214dcd95e0daeda82dc9ed
Dan Ibanez <daibane@sandia.gov>
2022-11-07 14:31:31 -0700

    technically MPI needs to be set up for P3A

--- p3a_unit_tests_main.cpp
+++ p3a_unit_tests_main.cpp
@@ -1,8 +1,10 @@
 #include <Kokkos_Core.hpp>
+#include <mpicpp.hpp>
 #include "gtest/gtest.h"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
+  mpicpp::environment mpi_state(&argc, &argv);
   Kokkos::ScopeGuard kokkos_library_state(argc, argv);
   return RUN_ALL_TESTS();
 }
